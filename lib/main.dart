3a6d1111391d4b5c0bfaceb2490edae391c93c78
Sanskar Tiwari <55942632+theindianappguy@users.noreply.github.com>
2020-02-09 18:16:31 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:myportfolio/landing_page.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LandingPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
